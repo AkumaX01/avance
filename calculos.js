a93758c975e6330141d24216673777ccaf82f39c
Hector Frausto Romo <117522560+AkumaX01@users.noreply.github.com>
2023-05-29 11:21:34 -0600

    Update calculos.js

--- calculos.js
+++ calculos.js
@@ -1,8 +1,9 @@
 module.exports = {
   area: function (width) {
-    return Math.pow(width, 2);
+  // return Math.pow(width, 2);
+    return ((Math.sqrt(3)*Math.pow(width,2)))/4;
   },
   perimetro: function (width) {
-    return width * 4;
+    return width * 3;
   },
 };
